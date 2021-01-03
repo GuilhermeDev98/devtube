200e18235316c99dee9d665248506b1c09e6c0a5
Fallitz <47279161+Fallitz@users.noreply.github.com>
2021-01-02 23:04:28 -0300

    Authentication

---
+++ src/authServer.js
@@ -0,0 +1,54 @@
+require("dotenv").config();
+
+const express = require('express')
+const app = express()
+const jwt = require('jsonwebtoken')
+
+app.use(express.json())
+
+let refreshTokens = []
+
+app.post('/token', (req, res) => {
+  const refreshToken = req.body.token
+  if (refreshToken == null) return res.sendStatus(401)
+  if (!refreshTokens.includes(refreshToken)) return res.sendStatus(403)
+  jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
+    if (err) return res.sendStatus(403)
+    const accessToken = generateAccessToken({ id : user.id})
+    res.json({ accessToken: accessToken })
+  })
+})
+
+app.delete('/logout', (req, res) => {
+  refreshTokens = refreshTokens.filter(token => token !== req.body.token)
+  res.sendStatus(204)
+})
+
+app.post('/login', (req, res) => {
+  // Authenticate User
+
+  const username = req.body.username
+  const password = req.body.password
+
+  if(req.body.username === 'Jim' && req.body.password === '0123456789'){ //Primeira verificação no Banco de dados
+  
+  const idUser = 2; //Verificar o ID no banco de dados
+  const user = { id: idUser };
+  const accessToken = generateAccessToken(user);
+  const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET)
+  refreshTokens.push(refreshToken)
+  res.json({ auth: true, accessToken: accessToken, refreshToken: refreshToken })
+
+  }
+
+  res.status(500).json({message: 'Login inválido!'});
+})
+
+function generateAccessToken(user) {
+  return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '60s' });
+}
+
+const PORT = 4000
+app.listen(PORT, () => {
+  console.info(`Server Running on Port ${PORT}`)
+})
--- src/index.js
+++ src/index.js
@@ -1,20 +1,76 @@
-require('dotenv').config()
+require("dotenv").config();
 
 const express = require('express')
 var cors = require('cors')
 const bodyParser = require('body-parser')
 
+const jwt = require("jsonwebtoken");
+
+
 const app = express()
 const routes = require('./routes')
 
-const PORT = process.env.APP_PORT | 3333
+const UsersController = require('./controllers/UsersController')
+
+const PORT = process.env.APP_PORT
 const APP_VERSION = process.env.APP_VERSION
 
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
-app.use(cors())
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors());
 
-app.use(`/api/${APP_VERSION}`, routes)
+app.use(`/api/${APP_VERSION}`, routes);
+
+
+
+/*================================ROUTER_AUTHENTICATION=================================*/
+app.use(express.json())
+
+const posts = [  //Exemplo de BD, remover assim que implementar o BD real
+  {
+    Id:1,
+    Email:'kyle@gmail.com',
+    Password: '0123456789',
+    Fullname: 'kyle smith',
+    Birth: '01/01/2021',
+    Nickname: 'Kyle',
+    StreamKey: 'keyStream',
+    ChannelId: '0486324',
+    CreatedAt: '',
+    UpdatedAt: ''
+  },
+  {
+    Id:2,
+    Email:'Jim@gmail.com',
+    Password: '0123456789',
+    Fullname: 'Jim Scot',
+    Birth: '01/01/2021',
+    Nickname: 'Jim',
+    StreamKey: 'keyStream',
+    ChannelId: '9566132',
+    CreatedAt: '',
+    UpdatedAt: ''
+  }
+]
+
+app.get('/posts', authenticateToken, (req, res) => {
+  res.json(posts.filter(post => post.Id === req.user.id)); //Enviar resultado do banco de dados para o usuário
+})
+
+function authenticateToken(req, res, next) {
+  const authHeader = req.headers['x-access-token'];
+  if (!authHeader) return res.status(401).json({ auth: false, message: 'No token provided.' });
+  console.log("Token:",authHeader);
+ 
+  jwt.verify(authHeader, process.env.ACCESS_TOKEN_SECRET, function(err, user){
+    console.log(err)
+    if (err) return res.status(500).json({ auth: false, message: 'Failed to authenticate token.' });
+   
+   // se tudo estiver ok, salva no request para uso posterior
+    req.user = user
+    next()
+  });
+}
 
 app.listen(PORT, () => {
     console.info(`Server Running on Port ${PORT}`)
